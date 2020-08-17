f464932eadaf4debf743e30f2fd373e1038a3ecd
qianjunxian <cherishtodaytime@163.com>
2020-08-17 16:05:23 +0800

    测试

---
+++ src/cn/zxr/idea/Demo.java
@@ -0,0 +1,27 @@
+package cn.zxr.idea;
+
+public class Demo {
+
+    public static  void main(String[] args)
+    {
+        System.out.println("aa");
+    }
+
+    /**
+     * 数量
+     */
+
+    public  int num;
+    /**
+     * 姓名
+     */
+    public  String name;
+    /**
+     * 年龄
+     */
+    public int age;
+    /**
+     * 班级
+     */
+    public String grade;
+}
